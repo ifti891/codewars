53ace3b1565326b256be140b093f45d43b37de77
iftikhar <iftikhar.sy@gmail.com>
2022-04-30 23:07:41 +0530

    Return Negative

---
+++ returnNegative.js
@@ -0,0 +1,14 @@
+// Return Negative
+
+function makeNegative(num) {
+  // Code?
+  if (num < 0 ){
+    return num
+  }else if (num > 0){
+    return -num
+  }else{
+    return 0
+  }
+}
+
+makeNegative(5);
