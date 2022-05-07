9da93f98c50034d8b4a81a8e1f333ea71c5dbf28
iftikhar <iftikhar.sy@gmail.com>
2022-05-07 23:07:24 +0530

    new challenge

---
+++ sumMixedArray.js
@@ -0,0 +1,11 @@
+// Sum Mixed Array
+
+function sumMix(x){
+  let output = 0;
+  for(let numbers of x){
+  output += parseInt(numbers); 
+  }
+  return output;
+}
+
+// JavaScripts actully sucks big balls
