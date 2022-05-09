e055269a268bfe2d9569fcec46d201d3b0239142
iftikhar <iftikhar.sy@gmail.com>
2022-05-09 23:08:21 +0530

    new codewars challenge

---
+++ reversedStrings.js
@@ -0,0 +1,11 @@
+// Reversed Strings
+
+function solution(str) {
+  //   let splitStr = str.split(" ");
+  //   let reverseStr = splitStr.reverse("");
+  //   let joinStr = reverseStr.join("");
+
+  //   return joinStr
+
+  return str.split("").reverse().join("")
+}
